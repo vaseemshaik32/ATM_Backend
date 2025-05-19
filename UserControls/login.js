77a6971352cadbc374cffaa5498da59af330ee01
markelof32 <vaseemshaik300@gmail.com>
2025-05-20 05:01:33 +0530

    d

--- UserControls/login.js
+++ UserControls/login.js
@@ -25,7 +25,7 @@ router.post('/login', async (req, res) => {
                 source:true, 
                 sameSite: 'Lax', 
                 path: '/', //all routes
-                domain: '.onrender.com',
+                domain: 'https://backend-544q.onrender.com',
                 maxAge: 3600000 // 1 hour expiration
             });
             await userstats.findOneAndUpdate({ userid: curuser._id }, { status: true });
